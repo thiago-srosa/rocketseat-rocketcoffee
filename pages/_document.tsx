869956896d00f2bc0d64a1d57b34a31c3dbff3f2
Thiago Rosa <thiago.rosa@jointecnologia.com.br>
2022-08-11 22:55:21 -0300

    Feat: Finished Project after third class

---
+++ pages/_document.tsx
@@ -0,0 +1,36 @@
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
+            rel="stylesheet"
+          />        
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,70 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div >
-      
-    </div>
-  )
-}
+    <>
+      <header>
+        <h1>
+          Rocket<span>Coffee</span>
+        </h1>
+        <p>Aberto todos os dias. 8h-21h</p>
+      </header>
 
-export default Home
+      <section>
+        <h2>Brunch</h2>
+        <ul>
+          <li>
+            <div className="details">
+              <h3>Waffle Doce</h3>
+              <p className="description">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum
+                atque.
+              </p>
+            </div>
+            <strong className="price">R$37.70</strong>
+          </li>
+
+          <li>
+            <div className="details">
+              <h3>Maça Doce</h3>
+              <p className="description">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum
+                atque.
+              </p>
+            </div>
+            <strong className="price">R$37.70</strong>
+          </li>
+        </ul>
+      </section>
+
+      <section>
+        <h2>Bebidas</h2>
+        <ul>
+          <li>
+            <div className="details">
+              <h3>Vinho</h3>
+              <p className="description">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum
+                atque.
+              </p>
+            </div>
+            <strong className="price">R$37.70</strong>
+          </li>
+
+          <li>
+            <div className="details">
+              <h3>Água com gás</h3>
+              <p className="description">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum
+                atque.
+              </p>
+            </div>
+            <strong className="price">R$37.70</strong>
+          </li>
+        </ul>
+      </section>
+    </>
+  );
+};
+
+export default Home;
